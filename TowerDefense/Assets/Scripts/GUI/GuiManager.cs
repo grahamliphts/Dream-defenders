295b222e628c60b4dadc4de3a2852bdb7505d54c
ravonyx <ravonyxa@gmail.com>
2015-01-26 00:39:55 +0100

    little fix in ui + load game when press button new game

---
+++ TowerDefense/Assets/Scripts/GUI/GuiManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiManager : MonoBehaviour 
+{
+
+	public void LoadGame()
+    {
+        Application.LoadLevel("MainScene"); 
+    }
+}
