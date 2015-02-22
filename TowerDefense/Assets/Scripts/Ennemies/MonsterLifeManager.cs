293dd509760408034ba3cc2b13cafdfc74c9aee0
pornin@hotmail.fr <banane32>
2015-02-22 11:11:00 +0100

    monster life management
    - aded initial life save (not working)

--- TowerDefense/Assets/Scripts/Ennemies/MonsterLifeManager.cs
+++ TowerDefense/Assets/Scripts/Ennemies/MonsterLifeManager.cs
@@ -6,7 +6,13 @@ public class MonsterLifeManager : MonoBehaviour
     public int life;
     public string[] _tag;
     public int[] _damage;
-    
+
+    private int life_save;
+    void start()
+    {
+        life_save = life;
+       // Debug.Log(life_save);
+    }
     void OnCollisionEnter(Collision col)
     {
         int count = 0;
@@ -23,6 +29,8 @@ public class MonsterLifeManager : MonoBehaviour
         {
            // Debug.Log(col.gameObject.name);
             //Probleme : cannot set active (false) because of the on trigger exit of tower
+            life = life_save;
+            //Debug.Log(life_save);
             transform.position = new Vector3(1000,1000, 1000);
             //transform.gameObject.SetActive(false);
         }
