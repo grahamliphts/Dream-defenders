d32ed44e17c4c3d15a60e37db45d3992754a88f8
pornin@hotmail.fr <banane32>
2015-03-30 13:06:55 +0200

    maj

---
+++ TowerDefense/Assets/Scripts/Player/PlayerRessourcesManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRessourcesManager : MonoBehaviour {
+
+	[SerializeField] int m_InitRessources;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
